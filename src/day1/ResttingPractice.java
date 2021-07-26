78bd0d710c6b2086216e1182c273cf5e9ec340cd
AbrahamWang <wang.ibrahim222@gmail.com>
2021-07-26 09:52:54 -0700

    test cases all done

--- src/day1/ResttingPractice.java
+++ src/day1/ResttingPractice.java
@@ -7,4 +7,12 @@ public class ResttingPractice {
     //work2
 
     //work3
+
+    //work4
+
+    //work5
+
+    //Mixed | Soft reset remove the commit from history but keep the local changes
+
+    // did part 4 and 5 and i am done in one commit
 }
